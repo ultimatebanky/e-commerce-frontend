466ac344c7af3bf53f9858dd59e5bb18d7b19bba
USER <user@USERs-MacBook-Pro.local>
2023-09-09 14:15:19 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
         {/* <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}></GoogleOAuthProvider> */}
           <Routes>
             <Route
-              path="/home"
+              path="/"
               element={
                 <Layout>
                   <Home />
